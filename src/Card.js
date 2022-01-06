88d9b57541d32219da6de36fe5192a95e60d1c6a
Mohamed Saleem <saleemilahi21@gmail.com>
2022-01-06 21:07:56 +0530

    updated Card

--- src/Card.js
+++ src/Card.js
@@ -6,7 +6,6 @@ const Card = ({ recipe }) => {
             <img src={recipe.recipe.image} loading="lazy" alt="food-img" />
             <h3>{recipe.recipe.label}</h3>
             <div className="btn">
-                <button>ingredient</button>
                 <a href={recipe.recipe.url} target="_blank" rel="noopener noreferrer">more details</a>
             </div>
         </div>
